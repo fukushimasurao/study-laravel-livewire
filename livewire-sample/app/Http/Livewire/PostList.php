5b8a0e48f9403877d376b0b294c8d5f4f021cf9c
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-03-19 20:54:56 +0900

    リアルタイム検索追加

--- livewire-sample/app/Http/Livewire/PostList.php
+++ livewire-sample/app/Http/Livewire/PostList.php
@@ -7,10 +7,16 @@ use Livewire\Component;
 
 class PostList extends Component
 {
+    public $word;
     public function render()
     {
+        $posts = Post::query()
+        ->when($this->word, fn($query, $value) => $query->where('title', 'LIKE', '%' . $value . '%'))
+        ->get();
+
         return view('livewire.post-list',[
-            'posts' =>Post::get(),
+            // 'posts' =>Post::get(),
+            'posts' => $posts,
         ]);
     }
 }
