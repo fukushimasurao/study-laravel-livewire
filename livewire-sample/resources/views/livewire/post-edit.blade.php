f05e82252991877135a45b0bd715d612fbd604b2
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-03-20 15:24:45 +0900

    modal化

--- livewire-sample/resources/views/livewire/post-edit.blade.php
+++ livewire-sample/resources/views/livewire/post-edit.blade.php
@@ -1,18 +1,18 @@
-<div x-data="{open: @entangle('showModal').defer}">
-
-    <input type="button" value="表示を切り替える" x-on:click="open = !open">
-    <form wire:submit.prevent="register" class="my-5 bg-red-100" x-show="open">
-        <div>
-            タイトル：<input type="text" wire:model.lazy="post.title">
-            {{-- ↓表示したりしなかったりするものは、divで囲む。詳しくはlaravelのトラブルシューティングにて。 --}}
-            <div>@error('post.title')<span style="color:red">{{ $message }}</span>@enderror</div>
-        </div>
-        <div>
-            本文：<textarea wire:model.lazy="post.body" cols="30" rows="5"></textarea>
-            <div>@error('post.body')<span style="color:red">{{ $message }}</span>@enderror</div>
-        </div>
-        <div>
-            <input type="submit" value="送信する">
-        </div>
-    </form>
+<div>
+    <x-modal wire:model="showModal">
+        <form wire:submit.prevent="register" class="my-5 bg-red-100">
+            <div>
+                タイトル：<input type="text" wire:model.lazy="post.title">
+                {{-- ↓表示したりしなかったりするものは、divで囲む。詳しくはlaravelのトラブルシューティングにて。 --}}
+                <div>@error('post.title')<span style="color:red">{{ $message }}</span>@enderror</div>
+            </div>
+            <div>
+                本文：<textarea wire:model.lazy="post.body" cols="30" rows="5"></textarea>
+                <div>@error('post.body')<span style="color:red">{{ $message }}</span>@enderror</div>
+            </div>
+            <div>
+                <input type="submit" value="送信する">
+            </div>
+        </form>
+    </x-modal>
 </div>
